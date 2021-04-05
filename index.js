7758edfc5d89d7fb6df2c066dbbe27547b92e94c
dino9611 <aldinorahman36@gmail.com>
2021-04-05 15:12:40 +0700

    async javascript

--- index.js
+++ index.js
@@ -1,18 +1,61 @@
-// const http = require("http");
-// const Mymodules = require("./modules");
-// const { addString, data, users } = Mymodules;
-// const server = http.createServer((req, res) => {
-//   //   console.log(req.url);
-//   if (req.url === "/") {
-//     res.writeHead(200, { "Content-type": "text/html" });
-//     res.end("<h1>halo gais </h1>");
-//   } else {
-//     res.writeHead(200, { "Content-type": "application/json" });
-//     res.end(JSON.stringify(users));
-//   }
-// });
+const http = require("http");
+const Mymodules = require("./modules");
+const { addString, data, users } = Mymodules;
+const fs = require("fs");
+// ! function return promise
+const tampilkan = (path) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, "utf-8", (err, datas) => {
+      if (err) {
+        reject("ini error");
+      } else {
+        resolve(datas);
+      }
+    });
+  });
+};
 
-// server.listen(5000, () => console.log("server on port 5000"));
+const server = http.createServer(async (req, res) => {
+  //   console.log(req.url);
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    // with promise async await
+    try {
+      const html = await tampilkan("./content/index.html");
+      console.log(typeof html);
+      console.log(html);
+      res.end(html.replace("{{name}}", data.nama));
+    } catch (error) {
+      console.log(error);
+    }
+    // promise original
+    // tampilkan("./content/index.html")
+    //   .then((res1) => {
+    //     console.log(typeof res1);
+    //     console.log(res1);
+    //     res.end(res1.replace("{{name}}", data.nama));
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+
+    //   !without promis
+    //  fs.readFile("./content/index.html", "utf-8", (err, datas) => {
+    //   if (err) {
+    //     console.log(err);
+    //     return;
+    //   }
+    //   console.log(typeof datas);
+
+    //   res.end(datas.replace("{{name}}", data.nama));
+    // });
+  } else {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(JSON.stringify(users));
+  }
+});
+
+server.listen(5000, () => console.log("server on port 5000"));
 
 // const { nama, isStudent } = require("./app");
 // const studentkah = require("./app").isStudent;
@@ -22,10 +65,16 @@
 // console.log(kalimat());
 // console.log(isStudent);
 
-const fs = require("fs");
-
 // const file = fs.readFileSync("./content/nama.txt", "utf-8");
 // const stream = fs.createReadStream("./content/nama.txt");
 // fs.mkdirSync("./content/foto");
-fs.renameSync("./content/iniaja.txt", "./content/foto/catetan.txt");
-console.log("success");
+// const fs = require("fs");
+
+// fs.readFile("./content/index.html", "utf-8", (err, data) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(data);
+//   console.log("sukses");
+// });
