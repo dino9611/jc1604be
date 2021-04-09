b7836aa6367c5e0fdd53a90a2401e8da5bbe9be7
dino9611 <aldinorahman36@gmail.com>
2021-04-09 15:18:33 +0700

    day 5 subqueries and views

--- databases/latsql.sql
+++ databases/latsql.sql
@@ -24,10 +24,21 @@ order by ratarata_cost desc limit 1 ;
 
 select * from payment;
 -- cari custumer id yang belanja paling banyak sampe terkecil urutkan
+select sum(amount) as belanja,customer_id 
+from payment group by customer_id order by belanja desc limit 1;
 -- cart staff-id yang rata2 penjualannya paling gede 
--- cari customer yang paling sering belanja 
+select sum(amount) as jual,staff_id 
+from payment group by staff_id order by jual desc limit 1;
+-- cari customer yang paling sering belanja
+select count(*) as total_belanja,customer_id 
+from payment group by customer_id order by total_belanja desc limit 1;
 -- hitung berpa pembayaran yang lebih dari 7 dolar
+select count(*) as jumlah_pembyaran
+from payment where amount >7;
 -- menggabungkan dua column firstname dan lastname
+select concat(first_name,' ',last_name) as fullname from actor;
+ 
+
 
 
 use belsql;
@@ -91,10 +102,57 @@ join products p on td.idproducts = p.id
 join transactions t on td.idtransaction= t.id 
 group by td.idtransaction;  
 
+-- subqueries
+
+select max(price) as price_max from (select td.*,p.name,p.description,t.status from transactions_details td 
+join products p on td.idproducts = p.id
+join transactions t on td.idtransaction= t.id) as tabgabung;
+
+select td.* from transactions_details td 
+join products p on td.idproducts = p.id
+join transactions t on td.idtransaction= t.id where t.status= 'onCart';  
 
 
+select * from transactions_details 
+where  idtransaction in (
+	select id  from transactions where status = 'onCart'
+)  ;
+
+select * 
+from transactions_details 
+where price > (select avg(price) as rata_rata from transactions_details) ;
 
 
+-- views
+-- panggil view
+select * from maxprice;
 
+-- gunainsakila
+use sakila;
+-- coba bikin list tiap actor peranin film apa saja
+select 
+concat(a.first_name,' ',a.last_name) as fullname, 
+group_concat( title order by title separator ' ; ') as list_film
+from film_actor fa 
+join actor a on fa.actor_id = a.actor_id 
+join film f on fa.film_id=f.film_id 
+group by fa.actor_id ;
+-- list actor untuk film dengan id=1
+select
+f.title,
+group_concat( concat(a.first_name, ' ', a.last_name ) order by a.first_name,a.last_name  separator ';') as list_actor
+from film_actor fa 
+join actor a on fa.actor_id = a.actor_id 
+join film f on fa.film_id=f.film_id 
+where f.film_id =1
+group by fa.film_id  ;
+
+-- ambil data film apa saja yang dimainkan penelope guiness
+--  tampilkan film yang paling laris
+ -- tampilkan data film genrenya animation 
+ -- hitung berapa jumlah film berdasarkan category urutkan dari yang terkecil sampai terbesar
+-- urutkan actor yang paling banyak membintangi film dari yang terbesar sampai yang terkecil
+-- urutkan customer yang paling banyak belanja terus berapa total dia ngabisin duitnya, 5 teratas ,tampilkan nama , total belanja
+-- cabang mana yang paling banyak menghasilkan duit , berapa duitnya
 
 
